0b4fe9ea58ad158e2dbcea86684f465e5a5254e1
andres <andres.david.mm@hotmail.com>
2026-04-24 16:55:03 -0500

    chore: setup tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { reactRouter } from "@react-router/dev/vite";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
-export default defineConfig({
-  plugins: [reactRouter()],
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    // 👇 solo cargar plugin fuera de test
+    mode !== "test" && reactRouter(),
+  ].filter(Boolean),
   resolve: {
     tsconfigPaths: true,
   },
-});
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./test/setup.ts",
+  },
+}));
