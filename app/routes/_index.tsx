01e26345da1a1a95258283ccdac27588cf6d08ae
andres <andres.david.mm@hotmail.com>
2026-04-24 15:18:53 -0500

    chore: switching to client side rendering

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,5 @@
-import { redirect } from "react-router";
-import type { Route } from "./+types/_index";
+import { Navigate } from "react-router";
 
-export async function loader({ request }: Route.LoaderArgs) {
-
-  return redirect("/login");
+export async function IndexPage() {
+  return <Navigate to="/login" />;
 }
