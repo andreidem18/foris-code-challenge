081b689b1e5dc55b51c08926a3b152d28bb28310
andres <andres.david.mm@hotmail.com>
2026-04-25 21:23:18 -0500

    feat(card): add flipped card styles and animations

--- app/assets/images/index.ts
+++ app/assets/images/index.ts
@@ -1,2 +1,3 @@
 export { default as logo } from "./logo.svg";
 export { default as googleIcon } from "./google-icon.svg";
+export { default as rickMortyVortex } from "./rick-morty-vortex.png";
--- app/features/game/components/memory-card/memory-card.tsx
+++ app/features/game/components/memory-card/memory-card.tsx
@@ -2,24 +2,43 @@ import { Image } from "~/ui";
 import type { Character } from "../../types/character";
 
 import styles from "./memory-card.module.scss";
+import { useState } from "react";
+import clsx from "clsx";
+import { rickMortyVortex } from "~/assets/images";
 
 interface Props {
   character: Character;
 }
 
 export const MemoryCard = ({ character }: Props) => {
+  const [isFlipped, setIsFlipped] = useState(true);
+
   return (
     <>
-      <div className={styles.card}>
-        <Image
-          src={character.image}
-          alt={`${character.name} image`}
-          className={styles.characterImage}
-          height={160}
-        />
-        <div className={styles.characterName}>{character.name}</div>
-        <div className={styles.characterDescription}>
-          {character.status} - {character.species}
+      <div
+        className={clsx(styles.card, isFlipped && styles.flipped)}
+        onClick={() => setIsFlipped(!isFlipped)}
+      >
+        <div className={styles.cardInner}>
+          <div className={clsx(styles.cardFace, styles.cardFront)}>
+            <Image
+              src={character.image}
+              alt={`${character.name} image`}
+              className={styles.characterImage}
+              height={160}
+            />
+            <div className={styles.characterName}>{character.name}</div>
+            <div className={styles.characterDescription}>
+              {character.status} - {character.species}
+            </div>
+          </div>
+          <div className={clsx(styles.cardFace, styles.cardBack)}>
+            <img
+              src={rickMortyVortex}
+              alt="back card"
+              className={styles.backCardImage}
+            />
+          </div>
         </div>
       </div>
     </>
