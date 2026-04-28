64f0b0251c10b95521e6243a70f7f3a438c28889
andres <andres.david.mm@hotmail.com>
2026-04-27 19:56:24 -0500

    feat(menu): add reanude game feature

--- app/routes/game/menu/menu.tsx
+++ app/routes/game/menu/menu.tsx
@@ -2,15 +2,28 @@ import { Button } from "~/ui/button/button";
 
 import styles from "./menu.module.scss";
 import { useNavigate } from "react-router";
+import { useGameStore } from "~/features/game/store/game-store";
 
 export default function MenuPage() {
   const navigate = useNavigate();
 
+  const { gameStarted, cards, resetGame } = useGameStore();
+
+  const newGame = () => {
+    resetGame();
+    navigate("/game/board");
+  };
+
   return (
     <div className={styles.menuContainer}>
       <div className={styles.menuContent}>
-        <Button onClick={() => navigate("/game/board")}>Nuevo juego</Button>
-        <Button disabled>Reanudar juego</Button>
+        <Button onClick={newGame}>Nuevo juego</Button>
+        <Button
+          disabled={!gameStarted || !cards}
+          onClick={() => navigate("/game/board")}
+        >
+          Reanudar juego
+        </Button>
         <Button variant="secondary">Mis puntajes</Button>
       </div>
     </div>
